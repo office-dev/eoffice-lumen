2c5ae0f97cf704b4d454ea55f7927e5e8fc7e3ae
Anthonius Munthi <me@itstoni.com>
2021-08-10 11:15:45 +0800

    fixing heroku build

--- src/Packages/Core/Providers/EOfficeServiceProvider.php
+++ src/Packages/Core/Providers/EOfficeServiceProvider.php
@@ -15,7 +15,6 @@ namespace EOffice\Packages\Core\Providers;
 
 use EOffice\Packages\Resource\Providers\ResourceServiceProvider;
 use EOffice\Packages\User\Providers\UserServiceProvider;
-use Flipbox\LumenGenerator\LumenGeneratorServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class EOfficeServiceProvider extends ServiceProvider
@@ -31,7 +30,7 @@ class EOfficeServiceProvider extends ServiceProvider
         $this->app->register(UserServiceProvider::class);
 
         if ('local' === env('APP_ENV', 'local')) {
-            $this->app->register(LumenGeneratorServiceProvider::class);
+            $this->app->register('Flipbox\LumenGenerator\LumenGeneratorServiceProvider');
         }
     }
 }
